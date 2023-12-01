55cd0db9b5b0b1ef8aa2f0d7fe95bb1e2083a92c
sojyo <tjtlswlgh@naver.com>
2023-12-01 23:53:55 +0900

    #21 vutify textfield

---
+++ src/components/common/STextField.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-text-field
+    v-model="value"
+    :label="label"
+    :type="type"
+    :rules="rules"
+    outlined
+    :required="required"
+  ></v-text-field>
+
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'nuxt-property-decorator'
+
+
+@Component({
+  name: 'STextField'
+})
+export default class STextField extends Vue {
+  @Prop({ type: String, default: '' }) label!: string;
+  @Prop({ type: String, default: 'text' }) type!: string;
+  @Prop({ type: Array, default: () => [] }) rules!: Array<Function>;
+  @Prop({ required: true }) value!: string;
+  @Prop({ required: true }) required!: string;
+
+}
+</script>
