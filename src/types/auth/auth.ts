489fc5777a1db140295653e979cd00c691e8e773
KurtDev599 <sw536880@naver.com>
2023-12-04 17:34:47 +0900

    #23 인터페이스 주석 추가

--- src/types/auth/auth.ts
+++ src/types/auth/auth.ts
@@ -1,13 +1,13 @@
 export interface IKakaoCertified {
-  apikey: string
-  redirectUri: string
-  code: string
+  apikey: string // api key
+  redirectUri: string // 콜백 uri
+  code: string // 인증 코드
 }
 
 export interface ISignUp {
-  email: string
-  pw: string
-  dateOfBirth: string
-  signUpChannel: string
-  nickName: string
+  email: string // 이메일
+  pw: string // 패스워드
+  dateOfBirth: string // 생년월일
+  signUpChannel: string // 가입채널
+  nickName: string // 닉네임
 }
