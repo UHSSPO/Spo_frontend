9fa719296e62ee35e9ab58cc18be53c5b0e38c82
KurtDev599 <sw536880@naver.com>
2023-12-19 16:41:15 +0900

    #31 typescript 경로 이슈

---
+++ shims-vue.d.ts
@@ -0,0 +1,16 @@
+declare module '*.vue' {
+  import Vue from 'vue'
+  export default Vue
+}
+
+// 추후 사용
+// declare module 'vue-pdf' {
+//   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+//   // @ts-ignore
+//   import pdf from 'vue-pdf'
+//   export default pdf
+// }
+//
+// declare module'@ckeditor/ckeditor5-vue2'
+//
+// declare module '@ckeditor/ckeditor5-build-decoupled-document'
