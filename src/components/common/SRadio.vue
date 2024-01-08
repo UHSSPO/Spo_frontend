a089ccafec1a24b37e01cfd64bcbc1e6f113a2aa
sojyo <tjtlswlgh@naver.com>
2024-01-08 14:06:37 +0900

    #26 vutify checkbox 생성, 그룹 생성, Radio까지 생성

---
+++ src/components/common/SRadio.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-container class="px-0" fluid>
+    <v-radio-group v-model="radioGroup">
+      <v-radio v-for="n in numberOfRadioButtons" :key="n" :label="`Radio ${n}`" :value="n" />
+    </v-radio-group>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      radioGroup: null,
+      numberOfRadioButtons: 5,
+    }
+  },
+}
+</script>
