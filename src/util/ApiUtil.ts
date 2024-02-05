1ae10e90217694d6f33e1414c5f81f04b62b7363
KurtDev599 <sw536880@naver.com>
2024-02-05 16:10:50 +0900

    minor fix

--- src/util/ApiUtil.ts
+++ src/util/ApiUtil.ts
@@ -5,27 +5,19 @@ import { Globals } from './Globals'
 import StringUtil from './StringUtil'
 
 export default class ApiUtil {
-  public static login(url: string, params: any): Promise<any> {
-    return $axios.post(url, params)
-  }
-
-  public static getCode(url: string, params: any): Promise<any> {
-    return $axios.$get(url, { params })
-  }
-
   public static get(url: string, params: any): Promise<any> {
     return $axios.$get(url, { params }).then(response => response.data)
   }
 
   public static post(url: string, params: any): Promise<any> {
-    return $axios.post(url, params).then(response => response.data)
+    return $axios.$post(url, params).then(response => response.data)
   }
 
   public static put(url: string, params: any): Promise<any> {
-    return $axios.put(url, params).then(response => response.data)
+    return $axios.$put(url, params).then(response => response.data)
   }
 
   public static delete(url: string, params: any): Promise<any> {
-    return $axios.delete(url, { params }).then(response => response.data)
+    return $axios.$delete(url, { params }).then(response => response.data)
   }
 }
