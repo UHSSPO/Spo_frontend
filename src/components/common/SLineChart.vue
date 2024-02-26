f53a41339bb82caf8e932b0db4b8de2f3e561346
sojyo <tjtlswlgh@naver.com>
2024-02-27 00:55:40 +0900

    #115 chart create

---
+++ src/components/common/SLineChart.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <s-line-chart
+      id="chart"
+      ref="lineChart"
+      :chart-data="chartData"
+      :options="chartOptions"
+    />
+  </div>
+</template>
+
+<script>
+import { Line } from 'vue-chartjs'
+
+export default {
+  extends: Line,
+  props: ['chartData', 'options'],
+  mounted() {
+    this.renderChart(this.chartData, this.options)
+  }
+}
+</script>
+
+<style scoped>
+</style>
