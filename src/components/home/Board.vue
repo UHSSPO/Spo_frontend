2536ef9cafafc217811e503a81e5ae1a700c5756
Han <hsw0914@naver.com>
2024-03-22 23:23:08 +0900

    #162 게시판 수정, 작성 작업완료. 조회, 댓글 작성, 수정, 조회 작업중.

--- src/components/home/Board.vue
+++ src/components/home/Board.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="boardWrap">
-    <h3>게시판</h3>
+    <div class="board-title-wrap">
+      <h3>게시판</h3>
+      <a @click="onClickToBoard(userInfo.userSequence)">게시물 작성하기</a>
+    </div>
     <ul class="boardListWrap">
       <li class="boardList">
         <div class="boardListWrap">
@@ -54,13 +57,34 @@
 
 <script lang="ts">
 
-import { Component, Vue } from 'nuxt-property-decorator'
+import { Component, namespace, Vue } from 'nuxt-property-decorator'
+import { commonStore } from '~/util/store-accessor'
+import { Namespace } from '~/util/Namespace'
+import { IUserDetail } from '~/types/auth/auth'
 
+const common = namespace(Namespace.COMMON)
 @Component({
   layout: 'empty',
   components: {}
 })
 export default class Board extends Vue {
+  /********************************************************************************
+ * Variables (Local, VUEX)
+ ********************************************************************************/
+
+  @common.State private token!: string
+  @common.State private userInfo!: IUserDetail
+
+  /********************************************************************************
+   * Method (Event, Business Logic)
+   ********************************************************************************/
+
+  private onClickToBoard(userSequence: number) {
+    commonStore.CHECK_LOGIN()
+    if (this.token) {
+      this.$router.push(`/board/boardwrite?userSequence=${userSequence.toString()}`)
+    }
+  }
 }
 
 </script>
