62c9cb56e81ebb0800e1e0124e344499d6773488
Han <hsw0914@naver.com>
2024-03-23 00:52:24 +0900

    #162 게시판 작업완료.

--- src/components/home/Board.vue
+++ src/components/home/Board.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="board-wrap">
-    <h3>게시판</h3>
+    <div class="board-title-wrap">
+      <h3>게시판</h3>
+      <a @click="onClickToBoardWrite(userInfo.userSequence)">게시물 작성하기</a>
+    </div>
     <ul class="board-list-wrap">
       <li class="board-list">
         <div class="board-list-wrap">
@@ -10,7 +13,7 @@
             <span>서서히 시작되리라 조심스레 예측합니다...</span>
           </div>
           <div>
-            <a href="#">더보기</a>
+            <a @click="onClickToBoard">더보기</a>
           </div>
           <div>4시간 전</div>
         </div>
@@ -21,7 +24,7 @@
             <span>서서히 시작되리라 조심스레 예측합니다...</span>
           </div>
           <div>
-            <a href="#">더보기</a>
+            <a @click="onClickToBoard">더보기</a>
           </div>
           <div>4시간 전</div>
         </div>
@@ -32,7 +35,7 @@
             <span>서서히 시작되리라 조심스레 예측합니다...</span>
           </div>
           <div>
-            <a href="#">더보기</a>
+            <a @click="onClickToBoard">더보기</a>
           </div>
           <div>4시간 전</div>
         </div>
@@ -43,7 +46,18 @@
             <span>서서히 시작되리라 조심스레 예측합니다...</span>
           </div>
           <div>
-            <a href="#">더보기</a>
+            <a @click="onClickToBoard">더보기</a>
+          </div>
+          <div>4시간 전</div>
+        </div>
+        <div class="board-list-wrap">
+          <div>해우소</div>
+          <div class="board-listThird">
+            <p>준비는끝났다는얘기</p>
+            <span>서서히 시작되리라 조심스레 예측합니다...</span>
+          </div>
+          <div>
+            <a @click="onClickToBoard">더보기</a>
           </div>
           <div>4시간 전</div>
         </div>
@@ -54,13 +68,41 @@
 
 <script lang="ts">
 
-import { Component, Vue } from 'nuxt-property-decorator'
+import { Component, namespace, Vue } from 'nuxt-property-decorator'
+import { commonStore } from '~/util/store-accessor'
+import { Namespace } from '~/util/Namespace'
+import { IUserDetail } from '~/types/auth/auth'
 
+const common = namespace(Namespace.COMMON)
 @Component({
   layout: 'empty',
   components: {}
 })
 export default class Board extends Vue {
+  /********************************************************************************
+   * Variables (Local, VUEX)
+   ********************************************************************************/
+
+  @common.State private token!: string
+  @common.State private userInfo!: IUserDetail
+
+  /********************************************************************************
+   * Method (Event, Business Logic)
+   ********************************************************************************/
+
+  private onClickToBoardWrite(userSequence: number) {
+    commonStore.CHECK_LOGIN()
+    if (this.token) {
+      this.$router.push(`/board/boardwrite?userSequence=${userSequence.toString()}`)
+    }
+  }
+
+  private onClickToBoard() {
+    commonStore.CHECK_LOGIN()
+    if (this.token) {
+      this.$router.push('/board')
+    }
+  }
 }
 
 </script>
---
+++ src/pages/board/boardwrite.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="container" class="line">
+    <div class="content dynamic-layout">
+      <div class="board-write-wrap">
+        <div class="board-write-title">
+          <h1>게시글 작성하기</h1>
+        </div>
+        <div class="board-write-content">
+          <div class="board-write-group">
+            <s-text-field
+              label="제목"
+              max-length="20"
+              placeholder="제목을 입력해주세요!"
+              :required="true"
+              type="text"
+            />
+          </div>
+          <div class="board-write-group">
+            <textarea id="content" name="content" placeholder="게시글 내용을 입력하세요" required />
+          </div>
+          <div class="board-write-group board-button-wrap">
+            <button class="board-write-button">
+              게시글 작성하기
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, namespace, Vue } from 'nuxt-property-decorator'
+import STextField from '~/components/common/STextField.vue'
+import SButton from '~/components/common/SButton.vue'
+import { Namespace } from '~/util/Namespace'
+
+const common = namespace(Namespace.COMMON)
+@Component({
+  components: { STextField, SButton },
+  layout: 'empty',
+  name: 'login'
+})
+export default class boardWrite extends Vue {
+  /********************************************************************************
+   * Variables (Local, VUEX)
+   ********************************************************************************/
+
+  // private formData = {
+  //   title: ''
+  // } as ILogin
+
+  /********************************************************************************
+   * Method (Event, Business Logic)
+   ********************************************************************************/
+}
+
+</script>
