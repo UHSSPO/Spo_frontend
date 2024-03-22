8dfb45889bbd138f5ae700b717b0788a98f4e50d
Han <hsw0914@naver.com>
2024-03-22 20:38:02 +0900

    #150 나머지 validate 처리

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -314,12 +314,8 @@ export default class extends Vue {
   }
 
   private stockDetail(stockInfoSequence: number) {
-    if (StringUtil.isEmpty(this.token)) {
-      commonStore.ADD_DIALOG({
-        id: 'ERROR',
-        text: '로그인이 필요한 서비스입니다!'
-      })
-    } else {
+    commonStore.CHECK_LOGIN()
+    if (this.token) {
       this.search = ''
       this.$router.push({
         name: 'detail',
