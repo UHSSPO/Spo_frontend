6c5343049fe18c8847277988eb6c6dcd643b5473
Han <hsw0914@naver.com>
2024-03-22 20:25:58 +0900

    #158 게시판 에디터 개발중

---
+++ src/components/common/STextArea.vue
@@ -0,0 +1,101 @@
+<template>
+  <v-textarea
+    v-model="localValue"
+    :label="label"
+    :rules="localRules"
+    :required="required"
+    :disabled="disabled"
+    :single-line="singleLine"
+    :hide-details="hideDetails"
+    :append-icon="appendIcon ? 'mdi-close-circle' : ''"
+    value="This is clearable text."
+    @change="onChange"
+    @input="onInput"
+    @keypress="onkeypress"
+  />
+</template>
+
+<script lang="ts">
+import { Component, Emit, Prop, Ref, Vue, Watch } from 'nuxt-property-decorator'
+import StringUtil from '../../util/StringUtil'
+
+@Component({
+  name: 'STextArea'
+})
+export default class STextArea extends Vue {
+  @Prop() label?: string
+  @Prop({ type: Array, default: () => [] }) rules?: Array<Function>
+  @Prop() value!: string
+  @Prop({ default: false }) required?: boolean
+  @Prop() maxLength?: number
+  @Prop() disabled?: boolean
+  @Prop({ default: false }) singleLine?: boolean
+  @Prop({ default: false }) hideDetails?: boolean
+  @Prop({ default: false }) appendIcon?: boolean
+  @Prop() placeholder?: string
+  @Prop() private readonly counter?: number
+  @Prop(Number) private readonly minLength?: number
+  @Prop() private readonly keyFilterRegex?: any
+
+  private localValue = ''
+  private localRules: Array<Function> = []
+
+  @Watch('value', { immediate: true })
+  onValueChange(newValue: string) {
+    this.localValue = newValue
+  }
+
+  @Watch('value', { immediate: true })
+
+  @Watch('rules')
+  private onWatchRules(eventValue: Array<Function>) {
+    this.setRules()
+  }
+
+  @Emit('change')
+  private onChange(eventValue: any) {
+    return eventValue
+  }
+
+  @Emit('input')
+  private onInput(eventValue: any) {
+    return eventValue
+  }
+
+  @Emit('keypress')
+  private onkeypress(eventValue: any) {
+    if (this.keyFilterRegex && !this.keyFilterRegex.test(eventValue.key)) {
+      eventValue.preventDefault()
+      return
+    }
+    return eventValue
+  }
+
+  created(): void {
+    this.setRules()
+  }
+
+  private setRules() {
+    this.localRules = []
+
+    if (!this.disabled) {
+      // 필수 체크
+      if (this.required) {
+        this.localRules = [
+          (value: string) => {
+            return StringUtil.isNotEmpty(value) || '필수입니다.'
+          }
+        ]
+      }
+      // 최소 길이 체크
+      if (StringUtil.isNotEmpty(this.minLength)) {
+        this.localRules.push((value: string) => (value || '').length >= (this.minLength as number) || this.minLength + '자 이상 입력해 주세요')
+      }
+
+      if (this.rules) {
+        this.localRules = [...this.localRules, ...this.rules]
+      }
+    }
+  }
+}
+</script>
