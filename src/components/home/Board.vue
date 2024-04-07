b2a20a02ca309282cb704574827d7718ad137dbc
Han <hsw0914@naver.com>
2024-04-07 20:47:31 +0900

    #162 게시판 퍼블리싱

--- src/components/home/Board.vue
+++ src/components/home/Board.vue
@@ -1,67 +1,67 @@
 <template>
-    <div class="board-title-wrap">
-      <h3>게시판</h3>
-      <a @click="onClickToBoardWrite(userInfo.userSequence)">게시물 작성하기</a>
-    </div>
-    <ul class="board-list-wrap">
-      <li class="board-list">
-        <div class="board-list-wrap">
-          <div>해우소</div>
-          <div class="board-listThird">
-            <p>준비는끝났다는얘기</p>
-            <span>서서히 시작되리라 조심스레 예측합니다...</span>
-          </div>
-          <div>
-            <a @click="onClickToBoard">더보기</a>
-          </div>
-          <div>4시간 전</div>
+  <div class="board-title-wrap">
+    <h3>게시판</h3>
+    <a @click="onClickToBoardWrite(userInfo.userSequence)">게시물 작성하기</a>
+  </div>
+  <ul class="board-list-wrap">
+    <li class="board-list">
+      <div class="board-list-wrap">
+        <div>해우소</div>
+        <div class="board-listThird">
+          <p>준비는끝났다는얘기</p>
+          <span>서서히 시작되리라 조심스레 예측합니다...</span>
         </div>
-        <div class="board-list-wrap">
-          <div>해우소</div>
-          <div class="board-listThird">
-            <p>준비는끝났다는얘기</p>
-            <span>서서히 시작되리라 조심스레 예측합니다...</span>
-          </div>
-          <div>
-            <a @click="onClickToBoard">더보기</a>
-          </div>
-          <div>4시간 전</div>
+        <div>
+          <a @click="onClickToBoard">더보기</a>
         </div>
-        <div class="board-list-wrap">
-          <div>해우소</div>
-          <div class="board-listThird">
-            <p>준비는끝났다는얘기</p>
-            <span>서서히 시작되리라 조심스레 예측합니다...</span>
-          </div>
-          <div>
-            <a @click="onClickToBoard">더보기</a>
-          </div>
-          <div>4시간 전</div>
+        <div>4시간 전</div>
+      </div>
+      <div class="board-list-wrap">
+        <div>해우소</div>
+        <div class="board-listThird">
+          <p>준비는끝났다는얘기</p>
+          <span>서서히 시작되리라 조심스레 예측합니다...</span>
         </div>
-        <div class="board-list-wrap">
-          <div>해우소</div>
-          <div class="board-listThird">
-            <p>준비는끝났다는얘기</p>
-            <span>서서히 시작되리라 조심스레 예측합니다...</span>
-          </div>
-          <div>
-            <a @click="onClickToBoard">더보기</a>
-          </div>
-          <div>4시간 전</div>
+        <div>
+          <a @click="onClickToBoard">더보기</a>
         </div>
-        <div class="board-list-wrap">
-          <div>해우소</div>
-          <div class="board-listThird">
-            <p>준비는끝났다는얘기</p>
-            <span>서서히 시작되리라 조심스레 예측합니다...</span>
-          </div>
-          <div>
-            <a @click="onClickToBoard">더보기</a>
-          </div>
-          <div>4시간 전</div>
+        <div>4시간 전</div>
+      </div>
+      <div class="board-list-wrap">
+        <div>해우소</div>
+        <div class="board-listThird">
+          <p>준비는끝났다는얘기</p>
+          <span>서서히 시작되리라 조심스레 예측합니다...</span>
         </div>
-      </li>
-    </ul>
+        <div>
+          <a @click="onClickToBoard">더보기</a>
+        </div>
+        <div>4시간 전</div>
+      </div>
+      <div class="board-list-wrap">
+        <div>해우소</div>
+        <div class="board-listThird">
+          <p>준비는끝났다는얘기</p>
+          <span>서서히 시작되리라 조심스레 예측합니다...</span>
+        </div>
+        <div>
+          <a @click="onClickToBoard">더보기</a>
+        </div>
+        <div>4시간 전</div>
+      </div>
+      <div class="board-list-wrap">
+        <div>해우소</div>
+        <div class="board-listThird">
+          <p>준비는끝났다는얘기</p>
+          <span>서서히 시작되리라 조심스레 예측합니다...</span>
+        </div>
+        <div>
+          <a @click="onClickToBoard">더보기</a>
+        </div>
+        <div>4시간 전</div>
+      </div>
+    </li>
+  </ul>
   </div>
 </template>
 
@@ -102,6 +102,6 @@ export default class Board extends Vue {
       this.$router.push('/board')
     }
   }
-
+}
 
 </script>
