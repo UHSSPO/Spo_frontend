1622988a722c5572f4fb35e7925e7bc9ae8ca2e9
sojyo <tjtlswlgh@naver.com>
2024-04-30 02:46:07 +0900

    #189 개인추천 로딩중, 내용수정

--- src/pages/picklist.vue
+++ src/pages/picklist.vue
@@ -4,36 +4,24 @@
       <div class="rank-wrap">
         <div class="picklist-content">
           <h1>주식 추천</h1>
+          <div v-if="loading" class="loading">
+            로딩 중...
+          </div>
           <router-link v-for="(stock, index) in stockInfo" :key="index" :to="{ path: '/detail', query: { stockInfoSequence: stock.stockInfoSequence.toString() } }">
             <div class="card" @click="onClickToDetails(stock.stockInfoSequence)">
-              <h2>{{ stock.srtnCd }}</h2>
+              <h2>{{ stock.itmsNm }}</h2>
               <p>{{ stock.mrktCtg }}</p>
-              <p>{{ stock.itmsNm }}</p>
+              <h1 class="ItemValue">
+                {{ stockInfo2?.priceInfo?.clpr | setNumberComma }} <span>원
+                  <span v-if="stockInfo2?.priceInfo?.fltRt === 0" class="zero"><em>{{ stockInfo2?.priceInfo?.fltRt }}</em></span>
+                  <span v-else :class="{minus: stockInfo2?.priceInfo?.fltRt < 0, plus: stockInfo2?.priceInfo?.fltRt > 0}"><em>{{ stockInfo2?.priceInfo?.fltRt }}</em></span>
+                </span>
+              </h1>
             </div>
           </router-link>
           <router-link v-if="!surveyDone" to="/survey">
             성향 분석 바로 하러가기
           </router-link>
-          <!--                    <div v-if="loading">-->
-          <!--                      로딩 중...-->
-          <!--                    </div>-->
-          <!--                    <div v-else>-->
-          <!--                      <div v-if="error" class="error">-->
-          <!--                        {{ error }}-->
-          <!--                      </div>-->
-          <!--                      <div v-else>-->
-          <!--                        <div v-if="recommendations.length === 0">-->
-          <!--                          추천 로직을 실행 중입니다. 잠시만 기다려주세요...-->
-          <!--                        </div>-->
-          <!--                        <div v-else>-->
-          <!--                          <div v-for="(recommendation, index) in recommendations" :key="index" class="card">-->
-          <!--                            <h2>{{ recommendation.symbol }}</h2>-->
-          <!--                            <p>{{ recommendation.description }}</p>-->
-          <!--                            <p>추천: {{ recommendation.rating }}</p>-->
-          <!--                          </div>-->
-          <!--                        </div>-->
-          <!--                      </div>-->
-          <!--                    </div>-->
         </div>
       </div>
     </div>
@@ -50,9 +38,10 @@ import { commonStore } from '~/util/store-accessor'
 @Component
 export default class Picklist extends Vue {
   /********************************************************************************
-     * Variables (Local, VUEX)
-     ********************************************************************************/
+   * Variables (Local, VUEX)
+   ********************************************************************************/
   private stockInfo: ICommendPersonalStock[] = []
+  private stockInfo2 = {} as IStockInfo
   private loading = true
   private error = null as string | null
   private surveyDone = true
@@ -61,17 +50,28 @@ export default class Picklist extends Vue {
    * Life Cycle
    ********************************************************************************/
   async created() {
-    await this.getPicklist()
+    setTimeout(async () => {
+      await this.getPicklist()
+    }, 4000)
   }
 
   private async getPicklist() {
-    this.$nextTick(() => {
+    this.loading = true
+    try {
       this.$nuxt.$loading.start()
-    })
-    this.stockInfo = await getPicklist()
-    this.$nextTick(() => {
+      this.stockInfo = await getPicklist()
+      this.stockInfo2 = (await getPicklist()).map((index: IStockInfo) => ({
+        clpr: this.stockInfo2.priceInfo?.clpr.toString(),
+        fltRt: this.stockInfo2.priceInfo?.fltRt,
+      }))
+
+      this.loading = false
+    } catch (error) {
+      this.loading = false
+      console.error('Error while fetching picklist:', error)
+    } finally {
       this.$nuxt.$loading.finish()
-    })
+    }
   }
 
   private onClickToDetails(stockInfoSequence: number) {
